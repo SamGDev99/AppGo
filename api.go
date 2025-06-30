a6a75ccf543b74cd9cf7cc4ff93acad014dbd5a4
Samuel Gamarra <gestordesarrollo@latincosa.com>
2025-06-30 10:46:46 -0300

    Se crea api para exponer datos al front

--- api.go
+++ api.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	datos, err := obtenerRegistros()
+	if err != nil {
+		http.Error(w, "Error al obtener los registros de la BD: ", http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(datos)
+
+}
